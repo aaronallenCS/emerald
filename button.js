6e310a3596b4fd36a4e365a6f63990fdc9f82157
Aaron <imaaronacs@gmail.com>
2021-01-12 19:35:33 -0800

    first commit

--- button.js
+++ button.js
@@ -1,28 +1,11 @@
 function changeColor() {
   var blueDiv = document.getElementById("changeable");
-  blueDiv.style.backgroundColor = "lightgreen";
+  var backgroundChange = document.getElementById("changeBackground");
+  blueDiv.style.backgroundColor = "red";
+  backgroundChange.style.backgroundColor="purple";
 }
 
 function changeColorBack() {
   var changeableDiv = document.getElementById("changeable")
-  changeableDiv.style.backgroundColorg = 'blue'
-}
-
-function replaceWord() {
-  var wordable = document.getElementById('wordable');
-
-  fetch("https://wordsapiv1.p.rapidapi.com/words/?random=true", {
-    method: "GET",
-    headers: {
-      "x-rapidapi-key": "your-api-key-here",
-      "x-rapidapi-host": "api-host-here",
-    },
-  })
-    .then((response) => {
-      console.log(response);
-      response.json().then(json => wordable.innerText = json.word);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+  changeableDiv.style.backgroundColor = 'blue'
 }
